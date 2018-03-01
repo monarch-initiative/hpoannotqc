10e8d9b6bf8f6edb470138199000615779a1e1ca
pnrobinson <peter.robinson@charite.de>
2018-02-28 22:41:02 -0500

    merger, prototype.

--- src/main/java/org/monarchinitiative/hpoannotqc/mondo/ClosestMatch.java
+++ src/main/java/org/monarchinitiative/hpoannotqc/mondo/ClosestMatch.java
@@ -3,11 +3,9 @@ package org.monarchinitiative.hpoannotqc.mondo;
 import com.github.phenomics.ontolib.formats.hpo.HpoOntology;
 import com.github.phenomics.ontolib.ontology.data.TermId;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
-import static com.github.phenomics.ontolib.ontology.algo.OntologyAlgorithm.getParentTerms;
+import static com.github.phenomics.ontolib.ontology.algo.OntologyAlgorithm.*;
 
 /**
  * This class with serve to experiment with the best way of writing a function that will find the closest
@@ -30,27 +28,29 @@ public class ClosestMatch {
      * @param query
      * @return set of terms that all have path length k to the query term whereby k is the smallest k of any matching term
      */
-    public Set<TermId> closestTerms(TermId query) {
-        Set<TermId> matchingTerms = new HashSet<>();
+    public Map<TermId, Integer> closestTerms(TermId query) {
+        Map<TermId, Integer> matchingTerms = new HashMap<>();
         if (diseaseterms.contains(query)) {
-            matchingTerms.add(query);
+            matchingTerms.put(query, 0);
             return matchingTerms;
         }
-        this.level=0;
         Stack<TermId> stack=new Stack();
-        Set<TermId> seen = new HashSet<>();
+        Map<TermId, Integer> seen = new HashMap<>();
         stack.push(query);
-        seen.add(query);
+        seen.put(query,0);
         while ( ! stack.empty()) {
             TermId tid = stack.pop();
+            int level = seen.get(tid) + 1; // parent terms are one level more distant
             Set<TermId> parents = getParentTerms(ontology,tid);
             for (TermId id : parents) {
                 if (diseaseterms.contains(id)) {
-                    matchingTerms.add(id);
+                    if (! matchingTerms.containsKey(id)) {
+                        matchingTerms.put(id,level); // prevent previous seen term from getting wrong level!
+                    }
                 }
-                if (!seen.contains(id)) {
+                if (!seen.containsKey(id)) {
                     stack.push(id);
-                    seen.add(id);
+                    seen.put(id,level);
                 }
             }
             if (!matchingTerms.isEmpty()) {
@@ -58,7 +58,40 @@ public class ClosestMatch {
             }
         }
         // if we get here, there was no match and matchingTerms will be empty.
+        // look also in opposite direction
+        Map<TermId,Integer> opposites = getViceVersa(query);
+        for (TermId i : opposites.keySet()) {
+            matchingTerms.put(i,opposites.get(i));
+        }
         return matchingTerms;
-
     }
+
+
+    Map<TermId, Integer> getViceVersa(TermId query) {
+        Map<TermId, Integer> opposites = new HashMap<>();
+        for (TermId annot : diseaseterms) {
+            if (isSubclass(ontology, annot, query)) {
+                opposites.put(annot, 3);
+            }
+        }
+            if (opposites.size() > 0)
+                return opposites;
+
+            for (TermId annot2 : diseaseterms) {
+                if (termsAreSiblings(ontology, annot2, query)) {
+                    opposites.put(annot2, 4);
+                }
+            }
+            if (opposites.size() > 0)
+                return opposites;
+//            for (TermId annot3 : diseaseterms) {
+//                if (termsAreRelated(ontology, annot3, query)) {
+//                    opposites.put(annot3, 5);
+//                }
+//
+//            }
+            return opposites;
+        }
+
+
 }
--- src/main/java/org/monarchinitiative/hpoannotqc/mondo/Merger.java
+++ src/main/java/org/monarchinitiative/hpoannotqc/mondo/Merger.java
@@ -6,8 +6,8 @@ import com.github.phenomics.ontolib.formats.hpo.HpoOntology;
 import com.github.phenomics.ontolib.formats.hpo.TermIdWithMetadata;
 import com.github.phenomics.ontolib.ontology.data.TermId;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * This class takes two HPO disease definitions and merges them TODO more documentation
@@ -24,9 +24,62 @@ public class Merger {
         this.ontology=ontology;
     }
 
+    public Set<TermId> getSetOfPhenotypicAbnormalities(List<TermIdWithMetadata> timdlist) {
+        Set<TermId> set = new HashSet<>();
+        for (TermIdWithMetadata tmd : timdlist) {
+            set.add(tmd.getTermId());
+        }
+        return set;
+    }
+
 
     public HpoDiseaseWithMetadata merge() {
+        System.err.println("##########  D1 to D2  ###########");
+        mergeDirection1();
+        System.err.println("##########  D2 to D1  ###########");
+        mergeDirection2();
+
+
+
+        return null;
+    }
+
+
+
+    private void mergeDirection1() {
+        ClosestMatch closestMatch1 = new ClosestMatch( ontology, getSetOfPhenotypicAbnormalities(disease_1.getPhenotypicAbnormalities()));
+
+
         String name=String.format("%s/%s(merged)",disease_1.getName(),disease_2.getName() );
+        String dbase="MONDO";
+        String dbId="000042"; // need to get real MONDO id!
+        List<TermIdWithMetadata> phenotypicAbnormalities = new ArrayList<>();
+        List<TermId> modesOfInheritance  = new ArrayList<>();
+        List<TermId> notTerms  = new ArrayList<>();
+        List<TermIdWithMetadata> termIn1not2 = new ArrayList<>();
+        List<TermIdWithMetadata> termIn2not1 = new ArrayList<>();
+        for (TermIdWithMetadata tiwm : disease_2.getPhenotypicAbnormalities()) {
+            String label = ontology.getTermMap().get(tiwm.getTermId()).getName();
+            if (disease_1.isDirectlyAnnotatedTo(tiwm)) {
+                phenotypicAbnormalities.add(tiwm);
+                System.err.println(String.format("D1 & D2: %s[%s]" ,label, tiwm.getIdWithPrefix()));
+            } else {
+                Map<TermId,Integer> bestMatch = closestMatch1.closestTerms(tiwm.getTermId());
+                System.err.print(String.format("D1: %s[%s]\tD2: " ,label, tiwm.getIdWithPrefix()));
+                String labels=bestMatch.keySet().stream().map(tid -> String.format("%s[%s](d=%d)",
+                        ontology.getTermMap().get(tid).getName(),
+                        tid.getIdWithPrefix(),
+                        bestMatch.get(tid))).collect(Collectors.joining(";"));
+                System.err.println(labels);
+            }
+        }
+        return;
+    }
+    private void mergeDirection2() {
+        ClosestMatch closestMatch1 = new ClosestMatch( ontology, getSetOfPhenotypicAbnormalities(disease_2.getPhenotypicAbnormalities()));
+
+
+        String name=String.format("%s/%s(merged)",disease_2.getName(),disease_1.getName() );
         String dbase="MONDO";
         String dbId="000042"; // need to get real MONDO id!
         List<TermIdWithMetadata> phenotypicAbnormalities = new ArrayList<>();
@@ -38,12 +91,18 @@ public class Merger {
             String label = ontology.getTermMap().get(tiwm.getTermId()).getName();
             if (disease_2.isDirectlyAnnotatedTo(tiwm)) {
                 phenotypicAbnormalities.add(tiwm);
-                System.err.println(String.format("MATCH : %s[%s]" ,label, tiwm.getIdWithPrefix()));
+                System.err.println(String.format("D1 & D2: %s[%s]" ,label, tiwm.getIdWithPrefix()));
             } else {
-                System.err.println(String.format("NO direct MATCH : %s[%s]" ,label, tiwm.getIdWithPrefix()));
+                Map<TermId,Integer> bestMatch = closestMatch1.closestTerms(tiwm.getTermId());
+                System.err.print(String.format("D1: %s[%s]\tD2: " ,label, tiwm.getIdWithPrefix()));
+                String labels=bestMatch.keySet().stream().map(tid -> String.format("%s[%s](d=%d)",
+                        ontology.getTermMap().get(tid).getName(),
+                        tid.getIdWithPrefix(),
+                        bestMatch.get(tid))).collect(Collectors.joining(";"));
+                System.err.println(labels);
             }
         }
-        return null;
+        return;
     }
 
 
