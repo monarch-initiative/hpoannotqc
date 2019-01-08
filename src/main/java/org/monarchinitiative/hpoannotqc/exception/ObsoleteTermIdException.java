28dda28b2ece269a6d5829c9051c54c0761636d3
pnrobinson <peter.robinson@jax.org>
2019-01-08 17:46:32 -0500

    moving logic to phenol

--- src/main/java/org/monarchinitiative/hpoannotqc/exception/ObsoleteTermIdException.java
+++
@@ -1,5 +0,0 @@
-package org.monarchinitiative.hpoannotqc.exception;
-
-public class ObsoleteTermIdException extends HpoAnnotationModelException {
-    public ObsoleteTermIdException(String msg) {super(msg);}
-}
