6c2d4667610ab2efab565c738b418877e26138f8
Peter Robinson <peter.robinson@jax.org>
2021-07-07 19:19:36 -0400

    cleanup

--- src/main/java/org/monarchinitiative/hpoannotqc/analysis/DuplicateChecker.java
+++
@@ -1,8 +0,0 @@
-package org.monarchinitiative.hpoannotqc.analysis;
-
-/**
- * HPO disease models are not allowed to have the same Disease/HPO/Sex/Modifer/Onset combinations
- * This class takes the annotations of a disease model and indicates any duplicated entries
- */
-public class DuplicateChecker {
-}
