7c2f037c84007a9354436251c218e461dadf4190
iimpulse <Michael.Gargano@jax.org>
2023-02-28 17:49:21 -0700

    creating new mappings file with phenol

--- src/main/java/org/monarchinitiative/hpoannotqc/Main.java
+++ src/main/java/org/monarchinitiative/hpoannotqc/Main.java
@@ -8,15 +8,15 @@ import java.util.concurrent.Callable;
 
 
 /**
- * This application will perform Q/C on the HPO rare disease "small file" annotation files, will transform the
+ * This application will perform quality control on the HPO rare disease "small file" annotation files, will transform the
  * small files into an integrate "large" file (phenotype_annotation.tab), and print a report to file.
  * @author <a href="mailto:peter.robinson@jax.org">Peter Robinson</a>
  * @version 0.1.16 (2018-01-02)
  */
 @CommandLine.Command(name = "java -jar HpoAnnotQc.jar",
         mixinStandardHelpOptions = true,
-        version = "hpoannotqc 1.9.9",
-        description = "Variant-motif visualization tool.")
+        version = "1.9.12",
+        description = "Hpo Annotation Quality Control.")
 public class Main implements Callable<Integer> {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
@@ -27,7 +27,7 @@ public class Main implements Callable<Integer> {
                 addSubcommand("download", new DownloadCommand()).
                 addSubcommand("big-file", new BigFileCommand()).
                 addSubcommand("qc", new BigFileQcCommand()).
-                addSubcommand("gene2phen", new Genes2PhenotypesCommand());
+                addSubcommand("mappings", new HpoMappingsCommand());
         cline.setToggleBooleanFlags(false);
         if (args.length == 0) {
             // this will cause a help message to be shown if the user calls the
